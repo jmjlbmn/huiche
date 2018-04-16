93ae8e84c27b1acf6147ee4da53ffe0d95a7d0f2
jmjlbmn <jmjlbmn@163.com>
2018-04-16 16:28:08 +0800

    调整Search,去掉默认实现,去掉注解实现,改为必须重写get方法,PredicateBuilder改为Search内部类

--- huiche-core/huiche-core-service-impl/src/main/java/org/huiche/core/util/SearchUtil.java
+++ huiche-core/huiche-core-service-impl/src/main/java/org/huiche/core/util/SearchUtil.java
@@ -24,6 +24,7 @@ import java.util.List;
  * @author Maning
  */
 @UtilityClass
+@SuppressWarnings("unchecked")
 public class SearchUtil {
     private static final String STAR = "*";
     private static final String LINE = "_";
