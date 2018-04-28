6c26fff8db3f67f5679c5fef9e2a0e1383e98437
jmjlbmn <jmjlbmn@163.com>
2018-04-28 11:15:47 +0800

    Search里面添加条件拼装方法

--- huiche-core/huiche-core-service/src/main/java/org/huiche/core/search/Search.java
+++ huiche-core/huiche-core-service/src/main/java/org/huiche/core/search/Search.java
@@ -4,9 +4,11 @@ package org.huiche.core.search;
 import com.querydsl.core.types.ExpressionUtils;
 import com.querydsl.core.types.Predicate;
 import org.huiche.core.util.BaseUtil;
+import org.huiche.core.util.SearchUtil;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import javax.validation.constraints.Null;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
@@ -24,6 +26,67 @@ public interface Search {
      */
     @Nullable
     Predicate get();
+    /**
+     * 条件处理,自定义值的处理方式
+     *
+     * @param check 值检查
+     * @param op    操作方法
+     * @param val   值
+     * @param <T>   值类型
+     * @return 条件
+     */
+    @Nullable
+    default  <T> Predicate predicate(@Nonnull java.util.function.Predicate<T> check, @Nonnull Function<T, Predicate> op, @Nullable T val) {
+        return SearchUtil.predicate(check, op, val);
+    }
+
+    /**
+     * 条件处理,值非空条件条件
+     *
+     * @param op  操作方法
+     * @param val 值
+     * @param <T> 值类型
+     * @return 条件
+     */
+    @Null
+    default <T> Predicate predicate(@Nonnull Function<T, Predicate> op, @Nullable T val) {
+        return SearchUtil.predicate(op, val);
+    }
+
+    /**
+     * 用and组合多个条件
+     *
+     * @param predicate 多个条件
+     * @return 最终条件
+     */
+    @Nullable
+    default Predicate predicates(@Nonnull Predicate... predicate) {
+        return SearchUtil.predicates(predicate);
+    }
+
+    /**
+     * 等同于predicates
+     *
+     * @param predicate 多个条件
+     * @return 最终条件
+     * @see #predicates(Predicate...)
+     */
+    @Nullable
+    default Predicate and(@Nonnull Predicate... predicate) {
+        return SearchUtil.and(predicate);
+    }
+
+    /**
+     * or
+     *
+     * @param left  条件1
+     * @param right 条件2
+     * @return 最终条件
+     */
+    @Nullable
+    default Predicate or(@Nullable Predicate left, @Nullable Predicate right) {
+        return SearchUtil.or(left, right);
+    }
 
     /**
      * 条件构造器Builder
