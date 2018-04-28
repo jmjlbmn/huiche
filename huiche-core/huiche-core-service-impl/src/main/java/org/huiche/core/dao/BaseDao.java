7b629ed8d8b49e444b670cd4e6146dd62e497b01
jmjlbmn <jmjlbmn@163.com>
2018-04-28 10:54:44 +0800

    Dao里面添加条件拼装方法

--- huiche-core/huiche-core-service-impl/src/main/java/org/huiche/core/dao/BaseDao.java
+++ huiche-core/huiche-core-service-impl/src/main/java/org/huiche/core/dao/BaseDao.java
@@ -21,6 +21,7 @@ import org.huiche.core.page.PageRequest;
 import org.huiche.core.page.PageResponse;
 import org.huiche.core.util.DateUtil;
 import org.huiche.core.util.QueryDslUtil;
+import org.huiche.core.util.SearchUtil;
 import org.huiche.core.util.StringUtil;
 import org.huiche.core.validation.ValidOnlyCreate;
 import org.slf4j.Logger;
@@ -34,11 +35,13 @@ import javax.annotation.OverridingMethodsMustInvokeSuper;
 import javax.annotation.Resource;
 import javax.validation.ConstraintViolation;
 import javax.validation.Validator;
+import javax.validation.constraints.Null;
 import javax.validation.groups.Default;
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 /**
@@ -805,4 +808,65 @@ public abstract class BaseDao<T extends BaseEntity> {
         return pk().desc();
     }
 
+    /**
+     * 条件处理,自定义值的处理方式
+     *
+     * @param check 值检查
+     * @param op    操作方法
+     * @param val   值
+     * @param <T>   值类型
+     * @return 条件
+     */
+    @Nullable
+    protected static <T> Predicate predicate(@Nonnull java.util.function.Predicate<T> check, @Nonnull Function<T, Predicate> op, @Nullable T val) {
+        return SearchUtil.predicate(check, op, val);
+    }
+
+    /**
+     * 条件处理,值非空条件条件
+     *
+     * @param op  操作方法
+     * @param val 值
+     * @param <T> 值类型
+     * @return 条件
+     */
+    @Null
+    protected static <T> Predicate predicate(@Nonnull Function<T, Predicate> op, @Nullable T val) {
+        return SearchUtil.predicate(op, val);
+    }
+
+    /**
+     * 用and组合多个条件
+     *
+     * @param predicate 多个条件
+     * @return 最终条件
+     */
+    @Nullable
+    protected static Predicate predicates(@Nonnull Predicate... predicate) {
+        return SearchUtil.predicates(predicate);
+    }
+
+    /**
+     * 等同于predicates
+     *
+     * @param predicate 多个条件
+     * @return 最终条件
+     * @see #predicates(Predicate...)
+     */
+    @Nullable
+    protected static Predicate and(@Nonnull Predicate... predicate) {
+        return SearchUtil.and(predicate);
+    }
+
+    /**
+     * or
+     *
+     * @param left  条件1
+     * @param right 条件2
+     * @return 最终条件
+     */
+    @Nullable
+    protected static Predicate or(@Nullable Predicate left, @Nullable Predicate right) {
+        return SearchUtil.or(left, right);
+    }
 }
