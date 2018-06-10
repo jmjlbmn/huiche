cce23e700e0ddb2aafd0cef6557d14a72b2b8edd
Jmjlbmn <jmjlbmn@163.com>
2018-06-10 21:43:55 +0800

    调整StreamUtil

--- huiche-core/src/main/java/org/huiche/core/util/StreamUtil.java
+++ huiche-core/src/main/java/org/huiche/core/util/StreamUtil.java
@@ -4,7 +4,6 @@ import lombok.experimental.UtilityClass;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.EnumSet;
 import java.util.List;
 import java.util.Set;
 import java.util.function.BiConsumer;
@@ -20,62 +19,35 @@ import java.util.stream.Collector;
  */
 @UtilityClass
 public class StreamUtil {
-    private static final Set<Collector.Characteristics> CH_ID
-            = Collections.unmodifiableSet(EnumSet.of(Collector.Characteristics.IDENTITY_FINISH));
+    public static <T, R> Collector<T, List<T>, List<R>> toList(Function<List<T>, List<R>> finisher) {
+        return new Collector<T, List<T>, List<R>>() {
+            @Override
+            public Supplier<List<T>> supplier() {
+                return ArrayList::new;
+            }
 
-    public static <T, R> Collector<T, ?, List<R>> toList(Function<List<T>, List<R>> mapper) {
-        return new CollectorImpl<>(
-                ArrayList::new, List::add,
-                (left, right) -> {
+            @Override
+            public BiConsumer<List<T>, T> accumulator() {
+                return List::add;
+            }
+
+            @Override
+            public BinaryOperator<List<T>> combiner() {
+                return (left, right) -> {
                     left.addAll(right);
                     return left;
-                },
-                mapper,
-                CH_ID);
-    }
+                };
+            }
 
-    static class CollectorImpl<T, A, R> implements Collector<T, A, R> {
-        private final Supplier<A> supplier;
-        private final BiConsumer<A, T> accumulator;
-        private final BinaryOperator<A> combiner;
-        private final Function<A, R> finisher;
-        private final Set<Characteristics> characteristics;
+            @Override
+            public Function<List<T>, List<R>> finisher() {
+                return finisher;
+            }
 
-        CollectorImpl(Supplier<A> supplier,
-                      BiConsumer<A, T> accumulator,
-                      BinaryOperator<A> combiner,
-                      Function<A, R> finisher,
-                      Set<Characteristics> characteristics) {
-            this.supplier = supplier;
-            this.accumulator = accumulator;
-            this.combiner = combiner;
-            this.finisher = finisher;
-            this.characteristics = characteristics;
-        }
-
-        @Override
-        public BiConsumer<A, T> accumulator() {
-            return accumulator;
-        }
-
-        @Override
-        public Supplier<A> supplier() {
-            return supplier;
-        }
-
-        @Override
-        public BinaryOperator<A> combiner() {
-            return combiner;
-        }
-
-        @Override
-        public Function<A, R> finisher() {
-            return finisher;
-        }
-
-        @Override
-        public Set<Characteristics> characteristics() {
-            return characteristics;
-        }
+            @Override
+            public Set<Characteristics> characteristics() {
+                return Collections.unmodifiableSet(Collections.emptySet());
+            }
+        };
     }
 }
